3f3040ede310f581326e7c3b5cea2c40f847583e
Jamie McCarthy <jamie@mccarthy.vg>
2022-02-27 14:46:50 -0600

    Pick RSpec config options

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -1,7 +1,11 @@
 require "unreliable/version"
 
 RSpec.describe Unreliable, "version" do
-  it "is" do
+  it "is defined" do
     expect(Unreliable::VERSION).to be
+  end
+
+  it "is correct" do
+    expect(Unreliable::VERSION).to start_with "0.0."
   end
 end
