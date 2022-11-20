d8a3047fa228f9f730173f4a2e33f2e600f170f9
Jamie McCarthy <jamie@mccarthy.vg>
2022-11-19 20:04:18 -0600

    Don't randomize order when PK is fully covered

--- lib/unreliable/build_order.rb
+++ lib/unreliable/build_order.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Override ActiveRecord::QueryMethods.build_order to always append a final ORDER(RAND())
+# Override ActiveRecord::QueryMethods.build_order to append a final ORDER(RAND()) when necessary
 
 require "active_record/connection_adapters/abstract_adapter"
 
@@ -10,6 +10,8 @@ module Unreliable
       super(arel)
 
       return unless Unreliable::Config.enabled?
+      return if from_only_internal_metadata?(arel)
+      return if from_one_table_with_ordered_pk?(arel)
 
       case Arel::Table.engine.connection.adapter_name
       when "Mysql2"
@@ -25,6 +27,38 @@ module Unreliable
         raise ArgumentError, "unknown Arel::Table.engine"
 
       end
+    end
+
+    def from_only_internal_metadata?(arel)
+      # No need to randomize queries on ar_internal_metadata
+      arel.froms.map(&:name) == [ActiveRecord::Base.internal_metadata_table_name]
+    end
+
+    def from_one_table_with_ordered_pk?(arel)
+      # This gem isn't (yet) capable of determining if ordering is reliable when two or
+      # more tables are being joined.
+      return false if arel.ast.cores.first.source.is_a?(Arel::Nodes::JoinSource) &&
+        arel.ast.cores.first.source.right.present?
+      return false if arel.froms.count > 1
+
+      # If the single table's primary key's column(s) are covered by the order columns,
+      # return true and don't randomize the order.
+      (primary_key_columns(arel) - order_columns(arel)).empty?
+    end
+
+    def primary_key_columns(arel)
+      # primary_key returns a String if it's one column, an Array if two or more
+      [ActiveRecord::Base.connection.primary_key(arel.froms.first.name)].flatten
+    end
+
+    def order_columns(arel)
+      from_table_name = arel.froms.first.name
+      arel.orders
+        .select { |order| order.is_a? Arel::Nodes::Ordering } # Don't try to parse textual orders
+        .map(&:expr)
+        .select { |expr| expr.relation.name == from_table_name }
+        .map(&:name)
+        .map(&:to_s) # In Rails < 5.2, the order column names are symbols; >= 5.2, strings
     end
   end
 end
