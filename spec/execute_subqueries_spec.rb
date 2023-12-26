f2fa298f785a866461089a0611b68704773379d4
Jamie McCarthy <jamie@mccarthy.vg>
2023-12-26 08:35:13 -0700

    Add a TODO for a delete_all check

--- spec/execute_subqueries_spec.rb
+++ spec/execute_subqueries_spec.rb
@@ -20,4 +20,20 @@ RSpec.describe Dream do
     Dreamer.delete_all
     Dream.delete_all
   end
+
+  it "deleted some dreams and dreamers" do
+    expect(Dream.count).to eq(0)
+    DreamTest::SUBJECTS.shuffle.each { |subject| Dream.new(subject: subject).save! }
+    expect(Dream.all.to_a.size).to eq(DreamTest::SUBJECTS.size)
+    DreamTest::DREAMER_NAMES.shuffle.each do |name|
+      dream = Dream.find_by(subject: DreamTest::SUBJECTS.sample)
+      Dreamer.new(name: name, dream: dream).save!
+    end
+    expect(Dreamer.all.to_a.size).to eq(DreamTest::DREAMER_NAMES.size)
+    # TODO: delete_all for where(something) and ensure the remainder exist;
+    # ensure the sql generated for the delete_all does not do the "id"="id" thing
+  ensure
+    Dreamer.delete_all
+    Dream.delete_all
+  end
 end
