afb1ed84ef61cadb5254fe782d1007bc16dc4f15
Jamie McCarthy <jamie@mccarthy.vg>
2023-12-26 11:21:00 -0700

    Test delete_all by a referenced model

--- spec/execute_subqueries_spec.rb
+++ spec/execute_subqueries_spec.rb
@@ -3,6 +3,7 @@
 class DreamTest < UnreliableTest
   SUBJECTS = %w[fire air water earth life death].freeze
   DREAMER_NAMES = %w[Morpheus Cluracan Mervyn Gilbert Nuala].freeze
+  raise ArgumentError.new("DreamTest needs at least as many subjects as dreamers") if SUBJECTS.size < DREAMER_NAMES.size
   RESPONSE_COUNT = 10
 end
 
@@ -21,17 +22,24 @@ RSpec.describe Dream do
     Dream.delete_all
   end
 
-  it "deleted some dreams and dreamers" do
+  it "deletes some dreams and dreamers" do
     expect(Dream.count).to eq(0)
     DreamTest::SUBJECTS.shuffle.each { |subject| Dream.new(subject: subject).save! }
     expect(Dream.all.to_a.size).to eq(DreamTest::SUBJECTS.size)
-    DreamTest::DREAMER_NAMES.shuffle.each do |name|
-      dream = Dream.find_by(subject: DreamTest::SUBJECTS.sample)
-      Dreamer.new(name: name, dream: dream).save!
+
+    one_dream_subject_per_dreamer = DreamTest::SUBJECTS.shuffle
+    DreamTest::DREAMER_NAMES.shuffle.each do |dreamer_name|
+      expect(one_dream_subject_per_dreamer.size).to(satisfy { |v| v > 0 })
+      dream = Dream.find_by(subject: one_dream_subject_per_dreamer.shift)
+      Dreamer.new(name: dreamer_name, dream: dream).save!
     end
     expect(Dreamer.all.to_a.size).to eq(DreamTest::DREAMER_NAMES.size)
-    # TODO: delete_all for where(something) and ensure the remainder exist;
-    # ensure the sql generated for the delete_all does not do the "id"="id" thing
+    expect(Dream.all.pluck(:dreamer_id).compact.size).to eq(DreamTest::DREAMER_NAMES.size)
+
+    sample_dreamer = Dreamer.find_by(name: DreamTest::DREAMER_NAMES.sample)
+    expect(Dream.where(dreamer: sample_dreamer).to_a.size).to eq(1)
+    Dream.where(dreamer: sample_dreamer).delete_all
+    expect(Dream.all.size).to eq(DreamTest::SUBJECTS.size - 1)
   ensure
     Dreamer.delete_all
     Dream.delete_all
