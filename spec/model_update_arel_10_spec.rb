58ec4934d0f4d92114a5c4b0e725bd5e03196e07
Jamie McCarthy <jamie@mccarthy.vg>
2024-01-15 15:20:15 -0600

    Recognize update_manager behavior differs for mysql

--- spec/model_update_arel_10_spec.rb
+++ spec/model_update_arel_10_spec.rb
@@ -4,6 +4,15 @@
 # by build_arel. It returns an Arel::UpdateManager. This makes sure that internal call
 # assembles the update query correctly.
 
+# For three of the four tests in this file, we special-case MySQL to check for the
+# different query it produces. The reason MySQL is handled differently is described
+# in Arel 10 here:
+# https://github.com/rails/rails/blob/v7.1.2/activerecord/lib/arel/visitors/to_sql.rb#L924-L942
+# As it says, MySQL forbids using the *same* table in a subquery UPDATE:
+# https://dev.mysql.com/doc/refman/8.0/en/subquery-errors.html
+# Different tables are allowed though, as in our "update cats where id in (select owners..."
+# test below.
+
 module Unreliable
   class SqlTestingData
     class_attribute :update_manager_sql
@@ -27,26 +36,50 @@ RSpec.describe "update_manager" do
 
     # rubocop:disable Layout/SpaceInsideParens,Layout/DotPosition
 
-    # Single subquery: "update cats where id in (select cats where name=bar)"
+    # Single subquery for sqlite/postgresql: "update cats where id in (select cats where name=bar)"
+    # Direct update for mysql: "update cats where name=bar"
     Cat.where(name: "foo").update_all(name: "bar")
     expect(Unreliable::SqlTestingData.update_manager_sql).
-      to end_with(adapter_text("ORDER BY RANDOM())"))
+      to end_with(
+        case UnreliableTest.find_adapter
+        when "mysql2"
+          "ORDER BY RAND()"
+        else
+          adapter_text("ORDER BY RANDOM())")
+        end
+      )
 
-    # Double-nested subquery: "update cats where id in (select cats where id in (select owners where name=baz))"
+    # Double-nested subquery for sqlite/postgresql: "update cats where id in (select cats where id in (select owners where name=baz))"
+    # Single-nested for mysql: "update cats where id in (select owners where name=baz)"
     Cat.where( id: Owner.where(name: "bar") ).update_all(name: "baz")
     expect(Unreliable::SqlTestingData.update_manager_sql).
-      to end_with(adapter_text("ORDER BY RANDOM()) ORDER BY RANDOM())"))
+      to end_with(
+        case UnreliableTest.find_adapter
+        when "mysql2"
+          "ORDER BY RAND()"
+        else
+          adapter_text("ORDER BY RANDOM()) ORDER BY RANDOM())")
+        end
+      )
+
+    # Single ordered subquery for sqlite/postgresql: "update cats where id in (select cats where name=bar limit ?)"
+    # Direct update for mysql: "update cats where name=baz"
+    Cat.where(name: "bar").limit(1).update_all(name: "baz")
+    expect(Unreliable::SqlTestingData.update_manager_sql).
+      to match(
+        case UnreliableTest.find_adapter
+        when "mysql2"
+          'ORDER BY RAND\(\) LIMIT '
+        else
+          adapter_text('ORDER BY RANDOM\(\) LIMIT ')
+        end
+      )
 
     # Single ordered subquery: "update cats where id in (select cats where name=bar order by id limit ?)"
     # The presence of the primary-key order means Unreliable does not apply its own order.
     Cat.where(name: "bar").order(:id).limit(1).update_all(name: "baz")
     expect(Unreliable::SqlTestingData.update_manager_sql).
-      to end_with(adapter_text("ORDER BY \"cats\".\"id\" ASC LIMIT ?)"))
-
-    # Single ordered subquery: "update cats where id in (select cats where name=bar limit ?)"
-    Cat.where(name: "bar").limit(1).update_all(name: "baz")
-    expect(Unreliable::SqlTestingData.update_manager_sql).
-      to end_with(adapter_text("ORDER BY RANDOM() LIMIT ?)"))
+      to match(adapter_text('ORDER BY "cats"\."id" ASC LIMIT '))
 
     # rubocop:enable Layout/SpaceInsideParens,Layout/DotPosition
   ensure
