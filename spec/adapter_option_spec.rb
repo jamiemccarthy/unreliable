76e1637331bdc047b6cf8992171bd28792e42fc2
Jamie McCarthy <jamie@mccarthy.vg>
2024-01-15 15:38:17 -0600

    typofix

--- spec/adapter_option_spec.rb
+++ spec/adapter_option_spec.rb
@@ -2,6 +2,6 @@
 
 RSpec.describe "rspec adapter" do
   it "is as specified" do
-    expect(ActiveRecord::Base.connection.adapter_name.downcase).to eq(UnreliableTest.find_adapter!)
+    expect(ActiveRecord::Base.connection.adapter_name.downcase).to eq(UnreliableTest.find_adapter)
   end
 end
