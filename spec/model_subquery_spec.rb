6b7099005924487eda15fe104162e53814b202a7
Jamie McCarthy <jamie@mccarthy.vg>
2026-04-07 20:28:15 -0400

    Test fix

--- spec/model_subquery_spec.rb
+++ spec/model_subquery_spec.rb
@@ -3,16 +3,8 @@
 RSpec.describe Cat do
   it "randomly selects in main query and subquery" do
     # rubocop:disable Layout/SpaceInsideParens,Layout/DotPosition
-    expect( Cat.where(name: Cat.where(name: "foo")).to_sql ).
-      to end_with(
-        case UnreliableTest.find_adapter
-        when "sqlserver"
-          # SQL Server strips ORDER BY from IN subqueries; only the outer query gets NEWID()
-          ") ORDER BY NEWID()"
-        else
-          adapter_text( %q[WHERE "cats"."name" = 'foo' ORDER BY RANDOM()) ORDER BY RANDOM()] )
-        end
-      )
+    expect( Cat.where(name: Cat.where(name: "foo")).to_sql.gsub("  ", " ") ).
+      to end_with(adapter_text( %q[WHERE "cats"."name" = 'foo' ORDER BY RANDOM()) ORDER BY RANDOM()] ))
     # rubocop:enable Layout/SpaceInsideParens,Layout/DotPosition
   end
 end
